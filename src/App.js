162dd4b6f6edf0fb650b9eaedf04d98e39947a10
Krishnendu <krishnendu.kalyani1@gmail.com>
2022-06-15 11:50:10 +0530

    date format fix

--- src/App.js
+++ src/App.js
@@ -42,11 +42,17 @@ function App() {
 	}, [notes]);
 
   const addNote = (text) => {
-    const date = new Date();
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    let date = dd + '/' + mm + '/' + yyyy;
     const newNote = {
       id: nanoid(),
       text: text,
-      date: date.toLocaleDateString()
+      date: date
     }
     const newNotes = [...notes, newNote];
     setNotes(newNotes);
